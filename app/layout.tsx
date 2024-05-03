376a6c0a97e66227e09450c2a94b2a9b0aeb79e8
申柯秋 <438518174@qq.com>
2024-05-03 23:00:02 +0800

    新增抽屉

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,6 @@ import type { Metadata, Viewport } from 'next';
 import Script from 'next/script';
 
 import 'tailwindcss/tailwind.css';
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
 
 import './globals.css';
 import '../styles/global.css';
@@ -56,16 +41,7 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <Script
-        type="module"
-        src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.esm.js"
-        strategy="lazyOnload"
-      />
-      <Script
-        noModule
-        src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.js"
-        strategy="lazyOnload"
-      />
+
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Calendar1 from '@/components/pages/Calendar';
+import { MyDrawer } from "@/components/ui/drawer"
 
 
 export default async function Page() {
+
   return (
-    <Calendar1 />
+    <div>
+      <div className="flex justify-center">
+        <Calendar1 />
+      </div>
+      <div>
+        <MyDrawer />
+      </div>
+    </div>
   )
 }
